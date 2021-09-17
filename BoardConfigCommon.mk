f96851168442011b4cb1ba3c4a60d8918a95d5b7
anrui2032 <anrui2032@gmail.com>
2021-09-17 12:26:37 +0800

    msm8960-common: Import vendor security patch date from MIUI V9.2.3.0
    Signed-off-by: anrui2032 <anrui2032@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,7 +199,7 @@ include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2016-01-01
+VENDOR_SECURITY_PATCH := 2017-01-01
 
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
